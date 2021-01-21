99ef9db2d205e704fcce4c7145951b02c34d3da3
Dmitry Korotin <korotin.dev@gmail.com>
2021-01-21 09:55:27 +0000

    add registration

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
-int main() { 
-  std::cout << "Hey!";
+#include "network.hpp"
+#include "user.hpp"
+
+using namespace std;
+
+namespace {
+  void print_users(ostream& out, const vector<User::Login>& users) {
+    for (const auto& login : users) {
+      out << login << "\n";
+    }
+  }
+  void print_registered(ostream& out, const Network& net) {
+    out << "registered users: \n";
+    print_users(out, net.registered_users());
+  }
+
+  void print_connected(ostream& out, const Network& net) {
+    out << "connected users: \n";
+    print_users(out, net.connected_users());
+  }
+}  // namespace
+
+int main() {
+  User luke("Luke"), darth_vader("Darth Vader");
+  Network network;
+
+  network.register_user(luke);
+  network.connect_user(darth_vader);
+
+  print_registered(cout, network);
+  print_connected(cout, network);
+
   return 0;
 }
